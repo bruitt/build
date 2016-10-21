76649b1c7c05d9b9a8191d9e0de89206e45f7c7f
Pavel Pertsev <morhetz@gmail.com>
2016-10-21 11:03:55 +0100

    Init

---
+++ packages/lint-lib/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: [
+    'eslint-config-airbnb-base',
+    'eslint-config-airbnb-base/rules/strict'
+  ].map(require.resolve),
+  rules: {
+    'arrow-body-style': [ 'off', 'as-needed' ],
+    'array-bracket-spacing': [ 'error', 'always', { 'objectsInArrays': false, 'arraysInArrays': false } ],
+    'arrow-parens': [ 'error', 'always' ],
+    'comma-dangle': [ 'error', 'never' ],
+    'dot-notation': [ 'error', { 'allowKeywords': true, 'allowPattern': '^[A-Z]+[A-Za-z]+$' } ],
+    'global-require': 'warn',
+    'no-constant-condition': [ 'error', { 'checkLoops': false } ],
+    'no-mixed-operators': [ 'error', { 'allowSamePrecedence': true } ],
+    'no-underscore-dangle': [ 'error', { 'allowAfterThis': true } ],
+    'no-unused-vars': [ 'warn', { 'vars': 'local', 'args': 'after-used' } ],
+    'prefer-const': 'off',
+    'prefer-template': 'warn',
+    'quotes': [ 'error', 'single' ],
+    'semi': [ 'error', 'never' ],
+    'vars-on-top': 'off',
+
+    'import/no-extraneous-dependencies': [ 'off' ],
+    'import/no-mutable-exports': 'off'
+  }
+}
